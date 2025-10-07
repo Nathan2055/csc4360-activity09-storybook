c4f11f8f474c90633ee7fbd1263dd26ee9f4a953
Nathan Larsen <nlarsen@nathan2055.com>
2025-10-07 19:00:27 -0400

    Test a different way of implementing AudioPlayer

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/models/recipe.dart';
+import 'package:just_audio/just_audio.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Recipe recipe;
 
-  const DetailsScreen({super.key, required this.recipe});
+  DetailsScreen({super.key, required this.recipe});
+
+  AudioPlayer player1 = AudioPlayer();
+
+  void startAudio() {
+    player1.setAsset('sounds/dramatic-organ-a.wav');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +23,10 @@ class DetailsScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              FloatingActionButton(
+                onPressed: startAudio,
+                child: const Icon(Icons.play_arrow),
+              ),
               Text(
                 'Ingredients',
                 style: Theme.of(context).textTheme.titleLarge,
